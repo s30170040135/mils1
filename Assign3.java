48333806f9de1940ceb905bdf23af243594bb137
s30170040135 <64573060+s30170040135@users.noreply.github.com>
2020-06-24 09:36:55 +0530

    Add files via upload

---
+++ Assign3.java
@@ -0,0 +1,30 @@
+
+
+class Patient {
+	
+	private double height;
+	private double weight;
+	
+	public Patient(String name, double height, double weight) {
+		super();
+		
+		this.height = height;
+		this.weight = weight;
+	}
+	
+	public double BMI () {
+		// BMI = ( Weight in Pounds / ( Height in inches x Height in inches ) ) x 703
+		return ( weight / ( height * height ) ) * 703;
+	}
+}
+
+	
+public class Assign3 {
+	public static void main(String[] args) {
+		Patient patient = new Patient("Arpan Das", 177/2.54, 59*2.2);
+		
+		System.out.println(patient.BMI());
+
+	}
+
+}
